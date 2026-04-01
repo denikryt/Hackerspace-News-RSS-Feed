5f60082d609af429eb1025fa36286dcc9fcf58ab
denchik <danil.mirzoev@gmail.com>
2026-04-01 22:33:49 +0300

    Update about page content

--- tests/renderAboutPage.test.js
+++ tests/renderAboutPage.test.js
@@ -10,18 +10,10 @@ describe("renderAboutPage", () => {
     expect(html).toContain('<link rel="icon" href="/favicon.png" type="image/png" />');
     expect(html).toContain("<h1>About</h1>");
     expect(html).not.toContain('class="about-link-muted" href="/about/index.html"');
-    expect(html).toContain("Data Source");
-    expect(html).not.toContain("Source page:");
-    expect(html).toContain('href="https://wiki.hackerspaces.org/User%3AJomat#Spaces_with_RSS_feeds"');
-    expect(html).toContain("wiki.hackerspaces.org");
-    expect(html).toContain('href="https://github.com/denikryt/hackerspace-news-rss-feed"');
-    expect(html).not.toContain("__DATA_SOURCE_URL__");
-    expect(html).not.toContain("__SOURCE_CODE_URL__");
     expect(html).toContain('href="/index.html"');
     expect(html).toContain('href="/feed/index.html"');
     expect(html).toContain(">Feed<");
     expect(html).toContain('href="/authors/index.html"');
-    expect(html).toContain("This site aggregates publications");
     expect(html).toContain('class="page-copy page-copy--narrow about-copy"');
   });
 });
